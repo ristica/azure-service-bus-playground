2844aed5c70a3419f4e8ce2418f6a048dac072ca
aleksandar.r@gmx.at <aleksandar.r@gmx.at>
2016-05-19 11:18:52 +0200

    queue tab done

--- Azure.ServiceBus.Demo.Manager/Views/QueueUserControl.xaml.cs
+++ Azure.ServiceBus.Demo.Manager/Views/QueueUserControl.xaml.cs
@@ -2,8 +2,11 @@
 using Microsoft.ServiceBus;
 using Microsoft.ServiceBus.Messaging;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,21 +15,59 @@ namespace Azure.ServiceBus.Demo.Manager.Views
     public partial class QueueUserControl : UserControl
     {
         private static ObservableCollection<string> Queues = new ObservableCollection<string>();
-         
+        private static ObservableCollection<string> Messages = new ObservableCollection<string>();
+        private SynchronizationContext _sync;
+        private static QueueClient _client;
+
         public QueueUserControl()
         {
             InitializeComponent();
             this.CbQueues.ItemsSource = Queues;
+            this.LbMessages.ItemsSource = Messages;
+            this.Loaded += ViewLoaded;
+            this._sync = SynchronizationContext.Current;
+        }
+
+        private void ViewLoaded(object sender, RoutedEventArgs e)
+        {
+            if (Queues.Any())
+            {
+                return;
+            }
+
+            var manager = this.GetNamespaceManager();
+            var queues = manager.GetQueues();
+
+            foreach ( var q in queues)
+            {
+                Queues.Add(q.Path);
+            }
         }
 
         private void BtnDeleteQueueClicked(object sender, RoutedEventArgs e)
         {
-            var selectedQueue = this.CbQueues.SelectedItem;
+            var selectedQueue = this.CbQueues.SelectedItem as string;
             if (selectedQueue == null)
             {
                 MessageBox.Show("Please chosse the queue to delete.");
                 return;
             }
+
+            if (_client != null && !_client.IsClosed)
+            {
+                _client.Close();
+            }
+
+            this.GetNamespaceManager().DeleteQueue(selectedQueue);
+
+            if (Queues.Contains(selectedQueue))
+            {
+                Queues.Remove(selectedQueue);
+            }
+
+            Messages.Clear();
+
+            this.ShowPrompt("deleted", selectedQueue);
         }
 
         private void BtnCreateQueueClicked(object sender, RoutedEventArgs e)
@@ -44,8 +85,7 @@ namespace Azure.ServiceBus.Demo.Manager.Views
                 DefaultMessageTimeToLive = new TimeSpan(0, 5, 0)
             };
 
-            var connectionString = CloudConfigurationManager.GetSetting(Common.AccessData.ServiceBusConfigConnectionStringName);
-            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+            var namespaceManager = this.GetNamespaceManager();
 
             if (namespaceManager.QueueExists(queue))
             {
@@ -59,16 +99,108 @@ namespace Azure.ServiceBus.Demo.Manager.Views
                 Queues.Add(queue);
             }
             this.TxtQueueName.Text = string.Empty;
+
+            this.ShowPrompt("created", queue);
         }
 
         private void BtnSendMessageClicked(object sender, RoutedEventArgs e)
         {
+            var selectedQueue = this.CbQueues.SelectedItem as string;
+            if (string.IsNullOrWhiteSpace(selectedQueue))
+            {
+                MessageBox.Show("Please choose a queue.");
+                return;
+            }
+
             var message = this.TxtMessage.Text;
             if (string.IsNullOrWhiteSpace(message))
             {
                 MessageBox.Show("Please put a text to send.");
                 return;
             }
+
+            var connectionString = CloudConfigurationManager.GetSetting(Common.AccessData.ServiceBusConfigConnectionStringName);
+            var client = QueueClient.CreateFromConnectionString(connectionString, selectedQueue);
+
+            var m = new BrokeredMessage(message);
+            client.Send(m);
         }
+
+        private void CbQueuesSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedQueue = this.CbQueues.SelectedItem as string;
+            Messages.Clear();
+            ListenToTheQueue(selectedQueue);
+        }
+
+        #region Helpers
+
+        private NamespaceManager GetNamespaceManager()
+        {
+            var connectionString = CloudConfigurationManager.GetSetting(Common.AccessData.ServiceBusConfigConnectionStringName);
+            return NamespaceManager.CreateFromConnectionString(connectionString);
+        }
+
+        private void ListenToTheQueue(string queue)
+        {
+            if (string.IsNullOrWhiteSpace(queue))
+            {
+                return;
+            }
+
+            Task.Run(() =>
+            {
+                var connectionString = CloudConfigurationManager.GetSetting(Common.AccessData.ServiceBusConfigConnectionStringName);
+                _client = QueueClient.CreateFromConnectionString(connectionString, queue);
+                var options = new OnMessageOptions
+                {
+                    AutoComplete = false,
+                    AutoRenewTimeout = TimeSpan.FromMinutes(1)
+                };
+                _client.OnMessage((message) =>
+                {
+                    try
+                    {
+                        this._sync.Send(arg =>
+                        {
+                            var m = message.GetBody<string>();
+
+                            Debug.Print("\tBody:          " + m);
+                            Debug.Print("\tMessageID:     " + message.MessageId);
+                            Debug.Print("");
+
+                            Messages.Add(m);
+
+                            this.TxtMessage.Text = string.Empty;
+
+                        }, null);
+
+                        // Remove message from queue.
+                        message.Complete();
+                    }
+                    catch (Exception)
+                    {
+                        // Indicates a problem, unlock message in queue.
+                        message.Abandon();
+                    }
+                }, options);
+            });
+        }
+
+        private void ShowPrompt(string message, string queue)
+        {
+            this.LblMessage.Content = string.Format("Queue '{0}' {1}...", queue, message);
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                this._sync.Send(arg =>
+                {
+                    this.LblMessage.Content = string.Empty;
+                }, null);
+            });
+        }
+
+        #endregion
+
     }
 }
